c5cc16bedb72444b62a9183221befec2df6ffca8
nika.pruidze <nika.pruidze1@gmail.com>
2024-06-05 23:58:03 +0400

    implement function to count occurence of each letter

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ def main():
         text = f.read()
         print(text)
     print(get_number_of_words(text))
+    print(count_letters_occurrence(text))
 
 def get_number_of_words(text):
     words = text.split()
     return len(words)
+
+def count_letters_occurrence(text):
+    letters = {}
+    for letter in text:
+        letterLower = letter.lower()
+        if letterLower in letters:
+            letters[letterLower] += 1
+        else:
+            letters[letterLower] = 1
+    return letters
 
 main()
