8f622b7b71ea000fda8a03a9eebf577e74a66318
nika.pruidze <nika.pruidze1@gmail.com>
2024-06-05 23:57:43 +0400

    add call to calculate number of words

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def main():
     with open("books/frankestein.txt") as f:
         text = f.read()
         print(text)
+    print(get_number_of_words(text))
 
 def get_number_of_words(text):
     words = text.split()
