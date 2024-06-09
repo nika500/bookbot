16a0b6dd2587d4296e56f9e18743ccf2b1f2413c
nika.pruidze <nika.pruidze1@gmail.com>
2024-06-09 19:59:32 +0400

    print a report

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
 def main():
-    with open("books/frankestein.txt") as f:
-        text = f.read()
-        print(text)
-    print(get_number_of_words(text))
-    print(count_letters_occurrence(text))
+    print("--- Begin report of books/frankenstein.txt ---")
+
+    file_path = "books/frankenstein.txt"
+    text = read_file(file_path)
+    number_of_words = get_number_of_words(text)
+    print(f"{number_of_words} words found in document\n")
+
+    letters = count_letters_occurrence(text)
+
+    lettersList = []
+    for key, value in letters.items():
+        if key.isalpha():
+            lettersList.append({"name": key, "num": value})
+
+    lettersList.sort(reverse=True, key=sort_on)
+    for letter in lettersList:
+        print(f"The '{letter['name']}' character was found {letter['num']} times")
+
+    print("--- End report ---")
+
+
+def sort_on(dict):
+    return dict["num"]
+
+
+def read_file(file_path):
+    with open(file_path) as f:
+        return f.read()
+
 
 def get_number_of_words(text):
     words = text.split()
     return len(words)
+
 
 def count_letters_occurrence(text):
     letters = {}
@@ -18,5 +43,6 @@ def count_letters_occurrence(text):
         else:
             letters[letterLower] = 1
     return letters
+
 
 main()
